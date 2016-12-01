ffae9c8efafcc08853ce35fd6a1005607e812482
vakst <vakst@vakst.tracker.k50.ru>
2016-12-01 20:47:47 +0300

    add function esample

--- app/DoctrineMigrations/Version20161201194217.php
+++ app/DoctrineMigrations/Version20161201194217.php
@@ -17,17 +17,26 @@ class Version20161201194217 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
 	$this->addSql('CREATE TABLE `Phone` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `group_id` int(11) DEFAULT NULL,
   `number` bigint(20) NOT NULL,
   `numberFormatted` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `priority` int(11) NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `sipDist` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_858EB8D9FE54D947` (`group_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 ');
+$this->addSql('CREATE FUNCTION WEIGHTED_AVERAGE (n1 INT, n2 INT, n3 INT, n4 INT)
+  RETURNS INT
+   DETERMINISTIC
+    BEGIN
+     DECLARE avg INT;
+     SET avg = (n1+n2+n3*2+n4*4)/8;
+     RETURN avg;
+    END'
+);
+
     }
 
     /**
